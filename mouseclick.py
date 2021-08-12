666b552e42206d38ba0f509e9d908a424a8329d7
mxy <mxy815@163.com>
2021-08-12 20:41:15 +0800

    全部改为自动识别

--- mouseclick.py
+++ mouseclick.py
@@ -1,12 +1,18 @@
 import  os
 import  time
 import  pyautogui as pg
+import  win32api
+import  win32con
+
 try:
     while True:
         sW, sH = pg.size()  #获取屏幕的尺寸（像素）screenWidth，screenHeight
         print("屏幕分辨率：\n"+str(sW)+','+str(sH)+'\n')  #打印屏幕分辨率
         x,y = pg.position()   #获取当前鼠标的坐标（像素）
+        win32api.mouse_event(win32con.MOUSEEVENTF_WHEEL,0,0,-1)
         print("鼠标坐标:\n" + str(x).rjust(4)+','+str(y).rjust(4)) #打印鼠标坐标值
+        pg.press('down')
+        print('down')
         time.sleep(2) #等待1秒
         os.system('cls')   #清屏
 except KeyboardInterrupt:
--- mouseclick2.py
+++ mouseclick2.py
@@ -59,16 +59,36 @@ def tell_njust_imhealthy():
     health_location = pyautogui.locateOnScreen('health_link.png', confidence=0.9)  # 匹配健康上报微信坐标
     healthy_x,healthy_y = pyautogui.center(health_location)
     m.click(int(healthy_x), int(healthy_y))  # 点链接
-    sleep(5)            # 点完链接多等会
-    m.click(950, 660)   # 点填报
+    sleep(10)            # 点完链接多等会
+    weixin_window_max_location = pyautogui.locateOnScreen('weixin_windowmax.png', confidence=0.9)  # 匹配窗口最大化坐标
+    weixin_window_max_x,weixin_window_max_y = pyautogui.center(weixin_window_max_location)
+    m.click(int(weixin_window_max_x), int(weixin_window_max_y))  # 点健康上报窗口最大化
+    sleep(2)
+    tianxie_location = pyautogui.locateOnScreen('kaishitianxie.png', confidence=0.9)  # 匹配开始填写坐标
+    tianxie_x,tianxie_y = pyautogui.center(tianxie_location)
+    m.click(int(tianxie_x), int(tianxie_y))  # 点填报
+    sleep(3.5)
+    m.click(int(tianxie_x), int(tianxie_y))  # 激活窗口
+    for i in range(0,20):
+        pyautogui.press('down')   
+    small_location = pyautogui.locateOnScreen('small.png', confidence=0.9)  # 匹配体温坐标
+    small_x,small_y = pyautogui.center(small_location)
+    m.click(int(small_x), int(small_y))  # 点
     sleep(1.5)
-    m.click(1150, 740)   # 点体温
+    no_location = pyautogui.locateOnScreen('no.png', confidence=0.9)  # 匹配否坐标
+    no_x,no_y = pyautogui.center(no_location)
+    m.click(int(no_x), int(no_y))  # 点
     sleep(1.5)
-    m.click(1150, 830)   # 点咳嗽
+    njust_zi_location = pyautogui.locateOnScreen('tijiao.png')  # 匹配提交坐标
+    njust_zi_x,njust_zi_y = pyautogui.center(njust_zi_location)
+    m.click(int(njust_zi_x), int(njust_zi_y))  # 点
     sleep(1.5)
-    m.click(1150, 893)   # 点上报
-    sleep(1.5)
-    weixin_window_min_location = pyautogui.locateOnScreen('weixin_windowmin.png', confidence=0.9)  # 匹配微信窗口最大化坐标
+    weixin_window_min_location = pyautogui.locateOnScreen('weixin_windowmin.png', confidence=0.9)  # 匹配微信窗口最xiao化坐标
+    weixin_window_min_x, weixin_window_min_y = pyautogui.center(weixin_window_min_location)
+    m.click(int(weixin_window_min_x),
+            int(weixin_window_min_y))  # 点微信窗口最小化(微信bug,最大化窗口时直接关闭，再打开时会处于小化状态，此时没有最大化图标，显示的是最小化图标)
+    sleep(2)
+    weixin_window_min_location = pyautogui.locateOnScreen('weixin_windowmin.png', confidence=0.9)  # 匹配微信窗口最xiao化坐标
     weixin_window_min_x, weixin_window_min_y = pyautogui.center(weixin_window_min_location)
     m.click(int(weixin_window_min_x),
             int(weixin_window_min_y))  # 点微信窗口最小化(微信bug,最大化窗口时直接关闭，再打开时会处于小化状态，此时没有最大化图标，显示的是最小化图标)
